b4e0bfa1ad050ac5e87b49e591983a5377d60844
affanadil <82828943+affanadil@users.noreply.github.com>
2026-08-13 19:29:08 +0500

    Triple

---
+++ Solve_With_Me_Series/Triple.cpp
@@ -0,0 +1,53 @@
+//     _    _____ _____ _    _   _      _    ____ ___ _
+//    / \  |  ___|  ___/ \  | \ | |    / \  |  _ \_ _| ||
+//   / _ \ | |_  | |_ / _ \ |  \| |   / _ \ | | | | || ||
+//  / ___ \|  _| |  _/ ___ \| |\  |  / ___ \| |_| | || |___
+// /_/   \_\_|   |_|/_/   \_\_| \_| /_/   \_\____/___|_____|
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+#define assault_rifle            \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+void solver()
+{
+    int n;
+    cin >> n;
+    vector<int> cnt(n + 1);
+    for (int i = 0; i < n; i++)
+    {
+        int k;
+        cin >> k;
+        cnt[k]++;
+    }
+    for (int i = 0; i < n + 1; i++)
+        if (cnt[i] >= 3)
+        {
+            cout << i << endl;
+            return;
+        }
+    cout << -1 << endl;
+}
+
+int main()
+{
+    assault_rifle;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solver();
+    }
+    return 0;
+}
