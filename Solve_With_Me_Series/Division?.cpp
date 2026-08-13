46bc130f51b771bfe9537bd9499c80507926fc7c
affanadil <82828943+affanadil@users.noreply.github.com>
2026-08-13 19:20:14 +0500

    Division
    This program classifies a given score into divisions based on specified thresholds.

--- Solve_With_Me_Series/Division?.cpp
+++ Solve_With_Me_Series/Division?.cpp
@@ -1,1 +1,47 @@
+//     _    _____ _____ _    _   _      _    ____ ___ _
+//    / \  |  ___|  ___/ \  | \ | |    / \  |  _ \_ _| ||
+//   / _ \ | |_  | |_ / _ \ |  \| |   / _ \ | | | | || ||
+//  / ___ \|  _| |  _/ ___ \| |\  |  / ___ \| |_| | || |___
+// /_/   \_\_|   |_|/_/   \_\_| \_| /_/   \_\____/___|_____|
 
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+#define assault_rifle            \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+void solver()
+{
+    int a;
+    cin >> a;
+    if (a < 1400)
+        cout << "Division 4\n";
+    else if (a < 1600)
+        cout << "Division 3\n";
+    else if (a < 1900)
+        cout << "Division 2\n";
+    else
+        cout << "Division 1\n";
+}
+
+int main()
+{
+    assault_rifle;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solver();
+    }
+    return 0;
+}
