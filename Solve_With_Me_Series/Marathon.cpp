0d0fbd017fc320b8c73120784051ae2aacfe7daf
affanadil <82828943+affanadil@users.noreply.github.com>
2026-08-15 19:30:29 +0500

    Marathon

---
+++ Solve_With_Me_Series/Marathon.cpp
@@ -0,0 +1,40 @@
+//     _    _____ _____ _    _   _      _    ____ ___ _
+//    / \  |  ___|  ___/ \  | \ | |    / \  |  _ \_ _| ||
+//   / _ \ | |_  | |_ / _ \ |  \| |   / _ \ | | | | || ||
+//  / ___ \|  _| |  _/ ___ \| |\  |  / ___ \| |_| | || |___
+// /_/   \_\_|   |_|/_/   \_\_| \_| /_/   \_\____/___|_____|
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+#define assault_rifle            \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+void solver()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << (b > a) + (c > a) + (d > a) << endl;
+}
+
+int main()
+{
+    assault_rifle;
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solver();
+    }
+    return 0;
+}
